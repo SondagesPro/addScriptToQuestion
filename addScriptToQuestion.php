105f9ad95dd0bd4b356979ee6992b8f260445a9b
Denis Chenu <denis@sondages.pro>
2016-11-17 16:21:12 +0100

    [license] GNU AGPL

--- addScriptToQuestion.php
+++ addScriptToQuestion.php
@@ -5,11 +5,11 @@
  *
  * @author Denis Chenu <denis@sondages.pro>
  * @copyright 2016 Denis Chenu <http://www.sondages.pro>
- * @license GPL v3
+ * @license AGPL v3
  * @version 0.1.0
  *
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
+ * it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
